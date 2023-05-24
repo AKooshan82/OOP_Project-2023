935c62ecc387957c96f0e89a152e13182a75eae5
hossein <itcyaghoubi@gmail.com>
2023-05-25 02:12:36 +0330

    all commands except show location & estimated time

--- src/RESTURANT.java
+++ src/RESTURANT.java
@@ -1,65 +1,92 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.SQLOutput;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.logging.Handler;
+
+import static java.lang.Thread.sleep;
 
 public class RESTURANT{
     String name;
     int ID;
     ADMIN owner;
+    RATING rating;
     ArrayList<USER> Costumers;
     ArrayList<String> type;
+    ArrayList<COMMENT> comments;
+    ArrayList<Integer> commentsIDs;
     ArrayList<FOOD> foods;
     ArrayList<FOOD> menu;
     ArrayList<Integer> foodIDs;
-    ArrayList<FOOD> activeOrders;
+    ArrayList<ORDER> activeOrders;
+    ArrayList<Integer> activeOrdersIDs;
+    ArrayList<ORDER> allOrders;
     RESTURANT(String name,ArrayList<String> type){
         this.name=name;
-        this.owner=SnappFood.nowAdmin;
-        this.ID=SnappFood.AllResturants.size()+1;
+        //this.owner=SnappFood.nowAdmin;
+        //this.ID=SnappFood.AllResturants.size()+1;
         this.foods=new ArrayList<>();
         this.foodIDs=new ArrayList<>();
         this.menu=new ArrayList<>();
         this.activeOrders=new ArrayList<>();
+        this.comments=new ArrayList<>();
+        this.commentsIDs=new ArrayList<>();
         this.type=type;
+        this.allOrders=new ArrayList<>();
+        this.activeOrdersIDs=new ArrayList<>();
+        this.rating=new RATING();
     }
     void newFood(FOOD food){
         this.foods.add(food);
         this.foodIDs.add(food.ID);
         this.menu.add(food);
     }
-    void deactiveFood(int ID){
+    boolean deactiveFood(int ID){
         int n=0;
+        boolean b=false;
         for(FOOD f : this.foods){
             if(f.ID==ID){
                 if(this.activeOrders.contains(f)){
                     System.out.println("your restaurant has some active orders of this food.please follow up them and then try again.");
+                    break;
                 }
                 else if(!f.isActive){
                     System.out.println("this food is already deactive.");
+                    break;
                 }
                 else {
                     n = this.menu.indexOf(f);
                     this.menu.remove(n);
                     f.isActive = false;
+                    b=true;
                     break;
                 }
             }
         }
+        return b;
     }
-    void activeFood(int ID){
+    boolean activeFood(int ID){
         int n=0;
+        boolean b=false;
         for(FOOD f : this.foods){
             if(f.ID==ID){
-                if(!f.isActive){
+                if(f.isActive){
                     System.out.println("this food is already active.");
+                    break;
                 }
                 else {
                     f.isActive = true;
                     this.menu.add(f);
+                    b=true;
                     break;
                 }
             }
         }
+        return b;
     }
     void deleteFood(int ID){
         int n=0;
@@ -74,20 +101,23 @@ public class RESTURANT{
             if(f.ID==ID){
                 this.foods.remove(f);
                 this.menu.remove(n1);
+                this.foodIDs.remove(this.foodIDs.indexOf(ID));
+                break;
             }
         }
     }
     void showFoods(){
+        Collections.sort(this.foods,FOOD.foodComparator);
         System.out.println("Foods:");
         for(FOOD f : this.foods){
             System.out.println(f.toString());}
     }
     void newFoodName(int ID,String  newName){
-        int n=this.foodIDs.indexOf(ID);
+        int n=this.getIndexOfFoodByID(ID);
         this.foods.get(n).name=newName;
     }
     void newFoodPrice(int ID,int price){
-        int n=this.foodIDs.indexOf(ID);
+        int n=this.getIndexOfFoodByID(ID);
         this.foods.get(n).price=price;
     }
     boolean isInActiveOrders(int ID){
@@ -102,15 +132,77 @@ public class RESTURANT{
         }
         return b;
     }
+    boolean discountFood(int ID,int percent, long time){
+        int n=this.getIndexOfFoodByID(ID);
+        boolean b=false;
+        if(this.foods.get(n).hasDiscount==null){
+            int t1=(int) (time*1000);
+            this.foods.get(n).hasDiscount=new Timed<>(Boolean.TRUE,Boolean.FALSE,time*1000);
+            this.foods.get(n).hasDiscount.tick();
+            this.foods.get(n).discount=percent;
+            this.setDiscountPercentRelatedToTime(this,n,t1);
+            b=true;
+        }
+        else {System.out.println("this food already has an active discount.");}
+        return b;
+    }
+    void setDiscountPercentRelatedToTime(RESTURANT resturant,int index,int time){
+        Timer timer = new Timer(time, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                resturant.foods.get(index).hasDiscount=null;
+                resturant.foods.get(index).discount=0;
+            }
+        });
+        timer.setRepeats(false); // Only execute once
+        timer.start(); // Go!
+    }
+    int getIndexOfFoodByID(int ID){
+        int n=-1;
+        for(FOOD f : this.foods) {
+            if (f.ID == ID) {
+                n= this.foods.indexOf(f);
+                break;
+                }
+            }
+        return n;
+    }
+    int getIndexOfFoodInMenuForUserByID(int ID){
+        int n=-1;
+        for(FOOD f : this.menu) {
+            if (f.ID == ID) {
+                n= this.foods.indexOf(f);
+                break;
+            }
+        }
+        return n;
+    }
+
     String showFoodType(){return this.type.toString();}
+
+    void displayActiveOrders(){
+        System.out.println("Active Orders:");
+        for(ORDER order : this.activeOrders){
+            order.showOrderDetailsForRestaurant();
+        }
+    }
+    void sendingOrder(int ID){
+     int index=this.activeOrdersIDs.indexOf(ID);
+     this.activeOrders.get(ID).isSent=true;
+     this.allOrders.add(this.activeOrders.get(index));
+     this.activeOrders.remove(index);
+     this.activeOrdersIDs.remove(index);
+        System.out.println("the order is sent successfully.");
+    }
     public static Comparator<RESTURANT> resturantComparator = new Comparator<RESTURANT>() {
         public int compare(RESTURANT r1, RESTURANT r2) {
-            if(!r1.equals(r2)) {
+            if(!r1.name.equals(r2.name)) {
                 return r1.name.compareTo(r2.name);
             }
             else{
                 return r1.ID-r2.ID;
             }
-        }};
+        }
+    };
 
 }
--- src/USER.java
+++ src/USER.java
@@ -1,33 +1,275 @@
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.regex.Matcher;
+
 public class USER {
     String username;
     String securityWord;
     String password;
-    USER(String username,String password){
-                this.username=username;
-                this.password=password;
+    FOOD nowUserFood;
+    RESTURANT nowUserResturant;
+    ArrayList<FOOD> cart;
+    ArrayList<ORDER> orders;
+    int balance;
 
-
+    USER(String username, String password) {
+        this.username = username;
+        this.password = password;
+        this.cart = new ArrayList<>();
+        this.orders = new ArrayList<>();
+        this.balance = 0;
     }
-    static boolean UserRegisterChecker(String username,String password){
-        boolean b=false;
-        if(!username.matches("^\\d*[a-zA-Z][a-zA-Z0-9]*$")) {System.out.println("username must be numbers or letters and should have at least 1 letter.");}
-        else if(SnappFood.UsersNames.contains(username)) System.out.println("username already exists.");
+
+    static boolean UserRegisterChecker(String username, String password) {
+        boolean b = false;
+        if (!username.matches("^\\d*[a-zA-Z][a-zA-Z0-9]*$")) {
+            System.out.println("username must be numbers or letters and should have at least 1 letter.");
+        } else if (SnappFood.UsersNames.contains(username)) System.out.println("username already exists.");
+        else {b=SnappFood.passwordChecker(password);}
+        return b;
+    }
+
+    void forgotPassword(String word) {
+        if (!this.securityWord.equals(word)) System.out.println("the answer is wrong.");
         else {
-            if(password.length()<8) System.out.println("password must contains at least 8 characters.");
-            else if(!password.matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{8,20}$")){
-                if(password.matches("^(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{8,20}$")) System.out.println("password must contains at least one number.");
-                else if(password.contains("//s")) System.out.println("white spaces don’t allowed in password.");
-                else if(password.matches("^(?=.*[0-9])(?=.*[A-Z])(?=\\S+$).{8,20}$")) System.out.println("password must contains at least one lower case alphabet.");
-                else if(password.matches("^(?=.*[a-z])(?=.*[0-9])(?=\\S+$).{8,20}$")) System.out.println("password must contains at least one upper case alphabet.");
+            System.out.println("your password is : " + this.password);
+        }
+    }
+
+    void showResturantsForUser(){
+        System.out.println("restaurants :");
+        for(RESTURANT r : SnappFood.AllResturants){
+            System.out.println("Restaurant Name: "+r.name+"  *  Restaurant ID: "+r.ID+"  * type: "+r.type.toString());
+        }
+    }
+    void searchRestaurantByName(Matcher matcher) {
+        String name = matcher.group("name");
+        boolean b = false;
+        for (RESTURANT resturant : SnappFood.AllResturants) {
+            if (resturant.name.equals(name)) {
+                System.out.println("Restaurant name: " + name + "  *  ID: " + resturant.ID + "  *  foodType: " + resturant.type.toString());
+                b = true;
             }
-            else{b=true;}
+        }
+        if (!b) System.out.println("Restaurant with the given name doesn't exist.");
+    }
+
+    boolean selectReataurant(Matcher matcher) {
+        int ID = Integer.parseInt(matcher.group("ID"));
+        boolean b = false;
+        if (SnappFood.AllResturants.size() < ID) System.out.println("Restaurant with the given ID doesn't exist.");
+        else {
+            this.nowUserResturant = SnappFood.AllResturants.get(ID - 1);
+            b = true;
         }
         return b;
     }
-    void forgotPassword(String word){
-        if(!this.securityWord.equals(word)) System.out.println("the answer is wrong.");
-        else{
-            System.out.println("your password is : "+this.password);
+    void showFoodsForUser(){
+        System.out.println("foods :");
+        for(FOOD f : this.nowUserResturant.menu){System.out.println(f.toStringForUser());}
+    }
+    void searchFoodByName(Matcher matcher) {
+        String name = matcher.group("name");
+        boolean b = false;
+        for (FOOD food : this.nowUserResturant.menu) {
+            if (food.name.equals(name)) {
+                System.out.println("Food name: " + name + "  *  ID: " + food.ID + "  *  Price: " + food.price);
+                b = true;
+            }
+        }
+        if (!b) System.out.println("Food with the given name doesn't exist.");
+    }
+
+    boolean selectFood(Matcher matcher) {
+        int ID = Integer.parseInt(matcher.group("ID"));
+        int n = this.nowUserResturant.getIndexOfFoodInMenuForUserByID(ID);
+        boolean b = false;
+        if (n == -1) System.out.println("Food with the given ID doesn't exist in Restaurant.");
+        else {
+            this.nowUserFood = this.nowUserResturant.menu.get(n);
+            b = true;
+        }
+        return b;
+    }
+    void chargeAccount(Matcher matcher) {
+        int amount = Integer.parseInt(matcher.group("amount"));
+        this.balance += amount;
+        System.out.println("account charged successfully.");
+    }
+
+    void showAccountCharge() {
+        System.out.println("balance: " + this.balance);
+    }
+
+    void addCommentForFood(String content) {
+        COMMENT comment = new COMMENT(content, this);
+        this.nowUserFood.comments.add(comment);
+        this.nowUserFood.commentsIDs.add(comment.ID);
+        System.out.println("comment with ID " + comment.ID + " added for this food successfully.");
+    }
+
+    boolean editCommentForFoodErrors(int ID) {
+        boolean b = true;
+        if (!this.nowUserFood.commentsIDs.contains(ID))
+            System.out.println("comment with the given ID doesn't exist for this food.");
+        else {
+            int index = this.nowUserFood.commentsIDs.indexOf(ID);
+            if (this.nowUserFood.comments.get(index).commentOwner != this) {
+                System.out.println("this comment is not yours.you can only edit your comments.");
+            } else {
+                b = false;
+            }
+        }
+        return b;
+    }
+
+    void editCommentForFoodConfirming(int ID, String newComment) {
+        int index = this.nowUserFood.commentsIDs.indexOf(ID);
+        this.nowUserFood.comments.get(index).editContent(newComment);
+    }
+
+    void displayCommentsForFood() {
+        System.out.println("comments:");
+        for (COMMENT comment : this.nowUserFood.comments) {
+            System.out.println("comment ID: " + comment.ID + " ----> " + comment.content);
+            comment.getCommentResponse();
         }
     }
+
+    void addCommentForRestaurant(String content) {
+        COMMENT comment = new COMMENT(content, this);
+        this.nowUserResturant.comments.add(comment);
+        this.nowUserResturant.commentsIDs.add(comment.ID);
+        System.out.println("comment with ID " + comment.ID + " added for this restaurant successfully.");
+    }
+
+    boolean editCommentForRestaurantErrors(int ID) {
+        boolean b = true;
+        if (!this.nowUserResturant.commentsIDs.contains(ID))
+            System.out.println("comment with the given ID doesn't exist for this restaurant.");
+        else {
+            int index = this.nowUserResturant.commentsIDs.indexOf(ID);
+            if (this.nowUserResturant.comments.get(index).commentOwner != this) {
+                System.out.println("this comment is not yours.you can only edit your comments.");
+            } else {
+                b = false;
+            }
+        }
+        return b;
+    }
+
+    void editCommentForRestaurantConfirming(int ID, String newComment) {
+        int index = this.nowUserResturant.commentsIDs.indexOf(ID);
+        this.nowUserResturant.comments.get(index).editContent(newComment);
+    }
+
+    void displayCommentsForRestaurant() {
+        System.out.println("comments:");
+        for (COMMENT comment : this.nowUserResturant.comments) {
+            System.out.println("comment ID: " + comment.ID + " ----> " + comment.content);
+            comment.getCommentResponse();
+        }
+    }
+    void addFoodToCart() {
+        this.cart.add(this.nowUserFood);
+        System.out.println("this food added to your cart successfully.");
+    }
+
+    void displayCartStatus() {
+        if (this.cart.size() == 0) System.out.println("there is no food in your cart.");
+        else {
+            System.out.println("your cart status:");
+            for (FOOD food : this.cart) {
+                System.out.println("Food ID: " + food.ID + "  *  name: " + food.name + "  *  price: " + food.price+"  *  discount: "+food.discount+"%");
+            }
+        }
+    }
+
+    void confirmOrder() {
+        ORDER order = new ORDER(this);
+        if(this.balance<order.totalCost) System.out.println("your balance is not enough.");
+        else {
+            this.balance-=order.totalCost;
+            this.orders.add(order);
+            this.nowUserResturant.activeOrders.add(order);
+            this.nowUserResturant.activeOrdersIDs.add(order.ID);
+            this.cart.clear();
+            System.out.println("order confirmed successfully.");
+        }
+    }
+
+    void showAllOrderHistory() {
+        if (this.orders.size() == 0) System.out.println("you don't have any orders until now;");
+        else {
+            for (ORDER order : this.orders) {
+                System.out.println("Order ID: " + order.ID + "  *  date & time: " + order.time);
+            }
+        }
+    }
+
+    void showSelectedOrderInformation(Matcher matcher) {
+        int ID = Integer.parseInt(matcher.group("ID"));
+        int index = this.getOrderIndexByID(ID);
+        if (index == -1) System.out.println("there is no order with the given ID.");
+        else {
+            System.out.println(this.orders.get(index).toString());
+        }
+    }
+
+    int getOrderIndexByID(int ID) {
+        int n = -1;
+        for (ORDER order : this.orders) {
+            if (order.ID == ID) {
+                n = this.orders.indexOf(order);
+                break;
+            }
+        }
+        return n;
+    }
+
+    //void diplayRestaurantRating(){}
+    void diplayFoodRating() {
+        if(this.nowUserFood.rating.usersRated.size()==0) System.out.println("no one rate this food yet.");
+        else{System.out.println(this.nowUserFood.rating.toString());}
+    }
+
+    void submitFoodRating(Matcher matcher) {
+        if(this.nowUserFood.rating.usersRated.contains(this)) System.out.println("yor have already rated this food before.");
+        else {
+            double amount = Double.parseDouble(matcher.group("amount"));
+            this.nowUserFood.rating.newRating(this, amount);
+            System.out.println("you rate this food successfully.");
+        }
+    }
+    void editFoodRating(Matcher matcher) {
+        if(!this.nowUserFood.rating.usersRatedNames.contains(this.username)) System.out.println("yor have not rated this food before.");
+        else {
+            double amount = Double.parseDouble(matcher.group("amount"));
+            this.nowUserFood.rating.editRating(this, amount);
+            System.out.println("your rate edited successfully..");
+        }
+    }
+    void submitRestaurantRating(Matcher matcher) {
+        if(this.nowUserResturant.rating.usersRated.contains(this)) System.out.println("yor have already rated this restaurant before.");
+        else {
+            double amount = Double.parseDouble(matcher.group("amount"));
+            this.nowUserResturant.rating.newRating(this, amount);
+            System.out.println("you rate this restaurant successfully.");
+        }
+    }
+    void editRestaurantRating(Matcher matcher) {
+        if(!this.nowUserResturant.rating.usersRatedNames.contains(this.username)) System.out.println("yor have not rated this restaurant before.");
+        else {
+            double amount = Double.parseDouble(matcher.group("amount"));
+            this.nowUserResturant.rating.editRating(this, amount);
+            System.out.println("your rate edited successfully..");
+        }
+    }
+    void diplayRestaurantRating() {
+        if(this.nowUserResturant.rating.usersRated.size()==0) System.out.println("no one rate this restaurant yet.");
+        else{System.out.println(this.nowUserResturant.rating.toString());}
+    }
+
 }
+
